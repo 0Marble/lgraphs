efe1cba8595fa68e741dc5252f94c5515fd972bc
Alexey Lobanov <zeromarblectm@gmail.com>
2023-05-08 17:38:29 +0800

    added a script for compiling the text

---
+++ gen-text.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# cd text
+for GRAPH_FILE in $(find graphs/ -name *.dot -printf '%f\n')
+do
+    echo $GRAPH_FILE
+    dot graphs/$GRAPH_FILE -Tpng -o images/$GRAPH_FILE.png
+done
+
+pdflatex main.tex
