7427fc5ae4edc5a724c545bd93a995fa75c23860
Alexey Lobanov <zeromarblectm@gmail.com>
2023-05-18 12:40:17 +0800

    changed graph images to svg

--- gen-text.sh
+++ gen-text.sh
@@ -4,7 +4,7 @@ cd text
 for GRAPH_FILE in $(find graphs/ -name *.dot -printf '%f\n')
 do
     echo $GRAPH_FILE
-    dot graphs/$GRAPH_FILE -Tpng -o images/$GRAPH_FILE.png
+    dot graphs/$GRAPH_FILE -Tsvg -o images/$GRAPH_FILE.svg
 done
 
-pdflatex main.tex
+pdflatex --shell-escape main.tex
